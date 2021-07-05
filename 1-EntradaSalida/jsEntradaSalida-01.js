1dcae96f4545293bf28f91ea5fd6557b6d2c0ef8
LeoLedesma <leofledesma@gmail.com>
2021-07-04 22:34:49 -0300

    clase1

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,11 @@
 /*
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
-function mostrar()
+function mostrar() 
 {
-	alert("Hola mundo 2020");
+	alert ("Esto funciona de maravilla");
+	//uso comillas cuando es texto
+	
+	
 }
 
