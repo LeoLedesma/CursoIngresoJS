e020b3ca54c7357d7c0c234901f3130d624f7fb6
LeoLedesma <86857633+LeoLedesma@users.noreply.github.com>
2021-07-22 22:28:15 -0300

    22 del 7

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,41 @@
+/**
+ * Bienvenidos.
+(SWITCH)pedir el ingreso de un día de la semana,
+si es fin de semana mostrar " buen finde",
+si es día hábil “ a trabajar”,
+si no es un día valido, también informarlo,
+usar una sola ventana alert.
+ */
 function mostrar()
 {
+var diaIngresado;
+var mensaje;
+
+diaIngresado=prompt("Ingrese un día de la semana:");
+diaIngresado= diaIngresado.toLowerCase();
+
+switch(diaIngresado)
+{
+    case "sabado":
+    case "domingo":
+        mensaje=" buen finde";
+        break;
+    case "lunes":
+    case "martes":
+    case "miercoles":
+    case "jueves":
+    case "viernes":
+        mensaje = " a trabajar";
+        break;
+    default:
+        mensaje = "No es un día valido."
+        break;
+}
+
+alert(mensaje);
+
+
+
 
 }
+
